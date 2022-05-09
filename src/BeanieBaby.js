a22d4c1d2a3b9e21ab11a6d4f8bb6455da18dc01
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-05-09 16:54:52 -0700

    page buttons working

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -5,7 +5,7 @@ export default function BeanieBaby({ beanieBaby }) {
   return (
     <div className='beanie-baby'>
       <Link to={`/beanies/${beanieBaby.id}`}>About</Link>
-      <h3>{beanieBaby.name}</h3>
+      <h3>{beanieBaby.title}</h3>
       <img src={beanieBaby.image} />
     </div>
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
