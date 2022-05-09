03c52b1c44766932169fd03f64306cb2473e4a94
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-05-09 12:34:24 -0700

    wrote Beanies List component

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
 
 export default function App() {
+  const [beanieBabies, setBeanBabies] =useState([]);
   return (
     <Router>
       <div className='App'>
         <Switch>
-          <Route>
+          <Route path='/'>
             {/* this home page route should list all the beanies */}
+
           </Route>
-          <Route> 
+          <Route path='/about'> 
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
           </Route>
         </Switch>
--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -1,8 +1,14 @@
 import BeanieBaby from './BeanieBaby.js';
+import { useState } from 'react';
+import React from 'react';
 
 export default function BeaniesList({ beanieBabies }) {
+  // const [beanieBabies, setBeanieBabies] = useState([]);
   return (
     <div className='beanies'>
       {/* render out the beanie babies as a list */}
+      {beanieBabies.map((beanieBaby, i) => 
+        <BeanieBaby key={`${beanieBaby.id}-${i}`} beanieBaby={beanieBaby} />
+      ) }
     </div>);
 }
