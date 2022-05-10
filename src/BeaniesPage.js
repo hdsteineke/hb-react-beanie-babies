a147f56b8d72332ee4aa9b6bbef429c1904d6934
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-05-09 17:00:34 -0700

    disabled previous page button at less than/equal to 1

--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -26,7 +26,7 @@ function App() {
       <div className='buttons'>
         {/* on click, this button should decrement the page in state  */}
         {/* also, disable this button when you are on the first page */}
-        <button onClick={() => setPage(page - 1)}>Previous Page</button>
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>Previous Page</button>
         {/* on click, this button should increment the page in state  */}
         <button onClick={() => setPage(page + 1)}>Next Page</button>
       </div>
