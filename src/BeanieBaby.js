1261b1ca53d6e8d30f48bf12dcf1575a830adb32
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-05-09 17:15:28 -0700

    made whole beanie baby clickable

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -4,9 +4,10 @@ import React from 'react';
 export default function BeanieBaby({ beanieBaby }) {
   return (
     <div className='beanie-baby'>
-      <Link to={`/beanies/${beanieBaby.id}`}>About</Link>
-      <h3>{beanieBaby.title}</h3>
-      <img src={beanieBaby.image} />
+      <Link to={`/beanies/${beanieBaby.id}`}>
+        <h3>{beanieBaby.title}</h3>
+        <img src={beanieBaby.image} />
+      </Link>
     </div>
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
